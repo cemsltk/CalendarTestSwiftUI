dc99a11e953355ffdcee22ade606efa8cda4983d
Cem Saltık <cemsltk35@gmail.com>
2022-07-04 15:07:56 +0300

    Initial Commit

---
+++ CalendarAppTest/CalendarAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppTestApp.swift
+//  CalendarAppTest
+//
+//  Created by Cem Saltık on 4.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
